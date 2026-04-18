f5c0416676af252e66c21d91e62e18a471a25121
Teyeb_sidi <teyebsidimbareck@gmail.com>
2026-04-18 16:45:56 +0000

    Add check-email path to user URLs

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
     path('register/',        RegisterView.as_view(),       name='register'),
     path('login/',           LoginView.as_view(),          name='login'),
     path('logout/',          LogoutView.as_view(),         name='logout'),
-    path('token/refresh/',   TokenRefreshView.as_view(),   name='token-refresh'),
-
+    path('token/refresh/',   TokenRefreshView.as_view(),   name='token-refresh'), 
+    path('check-email/', CheckEmailView.as_view(), name='check-email'),
     # Profile
     path('me/',              MeView.as_view(),             name='me'),
     path('change-password/', ChangePasswordView.as_view(), name='change-password'),
