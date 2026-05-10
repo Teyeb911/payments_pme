d65fc1bd3f163fa55752c702621930c97cc8e728
Teyeb_sidi <teyebsidimbareck@gmail.com>
2026-05-10 20:51:39 +0000

    Add SSL mode and connection timeout to DB options

--- config/settings.py
+++ config/settings.py
@@ -130,7 +130,11 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
         'HOST':     config('DB_HOST', default='localhost'),
         'PORT':     config('DB_PORT', default='5432'),
-        'OPTIONS': {'client_encoding': 'UTF8'},
+        'OPTIONS': {
+            'client_encoding': 'UTF8',
+            'sslmode': 'require',
+            'connect_timeout': 10,
+        },
     }
 }
 
