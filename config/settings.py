3a3a4380e06be751beff8c43ecd4f20959c829fc
Teyeb <teyebsidimbareck@email.com>
2026-05-12 22:29:30 +0000

    Console email backend in DEBUG, SMTP in production

--- config/settings.py
+++ config/settings.py
@@ -103,7 +103,7 @@ TEMPLATES = [
 ]
 
 # Email configuration
-EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND       = 'django.core.mail.backends.console.EmailBackend' if DEBUG else 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST          = 'smtp.gmail.com'
 EMAIL_PORT          = 587
 EMAIL_USE_TLS       = True
